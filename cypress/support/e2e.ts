011c6044ad605e2ef469eaf2ee32fefc638d9cec
fathima-A2023 <fathima.20211195@iit.ac.lk>
2025-07-06 11:32:18 +0530

    Use env variables to get the username and password

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -2,9 +2,11 @@
 import './commands';
 import LoginPage from "../pages/loginPage";
 
+const username = Cypress.env("username");
+const password = Cypress.env("password");
 const loginPage = new LoginPage();
 before('Xyicon Login',() =>{
-    cy.visit(Cypress.env('qaapp_url'));
-    loginPage.login('aaminaamanulla', '#Aaminaaman24225');
-    Cypress.config(); //to config defaultCommandTimeout to 60000 
+   cy.visit(Cypress.env('qaapp_url'));
+   loginPage.login(username, password);
+// Cypress.config(); //to config defaultCommandTimeout to 60000 
 })
