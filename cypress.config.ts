74747c65e9c242b404b9b00259119e9a1314a68f
fathima-A2023 <fathima.20211195@iit.ac.lk>
2025-07-06 11:33:47 +0530

    changes

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,9 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   defaultCommandTimeout: 60000,
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    // setupNodeEvents(on, config) {
+    //   implement node event listeners here
+    // },
     supportFile: 'cypress/support/e2e.ts'
   },
   env: {
